8b66c896bbe31b764a3596d669e87f7982b34903
mac <635442166@qq.com>
2020-04-14 16:32:30 +0800

    修改内容的 钜大核心技术能力

--- frontend/views/layouts/public/hexin_jishu_new.php
+++ frontend/views/layouts/public/hexin_jishu_new.php
@@ -3,5 +3,15 @@
         <div class="longline"></div>
         <div class="tabswitch-title-info"><h2 class="size1-9p">钜大核心技术能力</h2></div>
     </div>
-    <div class="img section60"><img src="/assets/images/hexin_jishu3.jpg" alt=""></div>
+    <div class="section60 row">
+        <div class="img1 img_item pull-left"><a href="/special/"><img src="/assets/images/hexin_jishu_small_01.jpg" alt="高能量密度/高倍率，高温/高电压"></a></div>
+
+        <div class="img1 img_item pull-left section110" style="width: 184px"><img class="center-block" src="/assets/images/hexin_jishu_small_add.jpg" alt=""></div>
+
+        <div class="img2 img_item pull-left"><a href="/diwen/"><img src="/assets/images/hexin_jishu_small_02.jpg" alt="低温充电，低温放电"></a></div>
+
+        <div class="img1 img_item pull-left section110" style="width: 184px"><img class="center-block" src="/assets/images/hexin_jishu_small_add.jpg" alt=""></div>
+
+        <div class="img1 img_item pull-left"><a href="/fanbao/"><img src="/assets/images/hexin_jishu_small_03.jpg" alt="满足EX ia\ib IIAIIB T1~T4防爆标准"></a></div>
+    </div>
 </section>
