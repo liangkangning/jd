e8cacbd61b007f52256fc43d837c1fcda3a4bbc7
mac <635442166@qq.com>
2020-04-14 15:01:27 +0800

    修改 核心技术 链接

--- frontend/views/layouts/public/hexin_jishu.php
+++ frontend/views/layouts/public/hexin_jishu.php
@@ -7,60 +7,67 @@
         <ul>
             <li class="col-md-4">
                 <div class="item relative">
-                    <div class="to_black">
-                        <div class="img"><img src="/assets/images/hexinjishu_1.jpg" alt=""></div>
-                        <div class="top_bg"></div>
-                        <div class="top"><p>4<span>高</span></p></div>
-                        <div class="text">
-                            <div class="title">高能量密度
-                                <p>285瓦时/公斤，700瓦时/升</p>
-                            </div>
-                            <div class="title">高倍率
-                                <p>100℃持续放电</p>
-                            </div>
-                            <div class="title">高温
-                                <p>80℃高温循环200周</p>
-                            </div>
-                            <div class="title">高电压
-                                <p>上限电压4.45V，平台电压3.85V</p>
+                    <a href="/special/">
+                        <div class="to_black">
+                            <div class="img"><img src="/assets/images/hexinjishu_1.jpg" alt=""></div>
+                            <div class="top_bg"></div>
+                            <div class="top"><p>4<span>高</span></p></div>
+                            <div class="text">
+                                <div class="title">高能量密度
+                                    <p>285瓦时/公斤，700瓦时/升</p>
+                                </div>
+                                <div class="title">高倍率
+                                    <p>100℃持续放电</p>
+                                </div>
+                                <div class="title">高温
+                                    <p>80℃高温循环200周</p>
+                                </div>
+                                <div class="title">高电压
+                                    <p>上限电压4.45V，平台电压3.85V</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
+
                 </div>
             </li>
             <li class="col-md-4">
                 <div class="item relative">
-                    <div class="to_black">
-                        <div class="img"><img src="/assets/images/hexinjishu_2.jpg" alt=""></div>
-                        <div class="top_bg"></div>
-                        <div class="top"><p>2<span>低</span></p></div>
-                        <div class="text">
-                            <div class="title">低温充电
-                                <p>-40℃低温0.2C充电</p>
-                            </div>
-                            <div class="title">
-                                低温放电
-                                <p>-50℃放电，容量保持75%以上</p>
+                    <a href="/diwen/">
+                        <div class="to_black">
+                            <div class="img"><img src="/assets/images/hexinjishu_2.jpg" alt=""></div>
+                            <div class="top_bg"></div>
+                            <div class="top"><p>2<span>低</span></p></div>
+                            <div class="text">
+                                <div class="title">低温充电
+                                    <p>-40℃低温0.2C充电</p>
+                                </div>
+                                <div class="title">
+                                    低温放电
+                                    <p>-50℃放电，容量保持75%以上</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </li>
             <li class="col-md-4">
                 <div class="item relative">
-                    <div class="to_black">
-                        <div class="img"><img src="/assets/images/hexinjishu_3.jpg" alt=""></div>
-                        <div class="top_bg"></div>
-                        <div class="top"><p>1<span>防爆</span></p></div>
-                        <div class="text">
-                            <div class="title">满足EX ia\ib IIA</div>
-                            <div class="title">
-                                IIB T1~T4防爆标准
-                                <p>200Wh/kg高能量密度<br>
-                                    改性三元化学体系</p>
+                    <a href="/fanbao/">
+                        <div class="to_black">
+                            <div class="img"><img src="/assets/images/hexinjishu_3.jpg" alt=""></div>
+                            <div class="top_bg"></div>
+                            <div class="top"><p>1<span>防爆</span></p></div>
+                            <div class="text">
+                                <div class="title">满足EX ia\ib IIA</div>
+                                <div class="title">
+                                    IIB T1~T4防爆标准
+                                    <p>200Wh/kg高能量密度<br>
+                                        改性三元化学体系</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </li>
 
