f0e2c4e8465bb2cdfd0aaffeb1d212bb873f164f
mac <635442166@qq.com>
2020-07-09 10:44:05 +0800

    新增邮件模板

---
+++ common/mail/layouts/other.php
@@ -0,0 +1,12 @@
+<h2><?= $title ?></h2>
+
+<div class="content">
+
+    <p>邮箱：<?= $mail ?></p>
+    <p>电话：<?= $phone ?></p>
+    <p>电压：<?= $dianya ?></p>
+    <p>电容：<?= $rongliang ?></p>
+    <p>尺寸：<?= $chicun ?></p>
+    <p>其他信息：<?= $message ?></p>
+
+</div>
