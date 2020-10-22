e3b7f1bff69795adc7e4ae365a79714271f2ad0b
mac <635442166@qq.com>
2020-10-22 09:35:32 +0800

    磷酸铁锂电池厂家 特殊文章修改

--- frontend/views/column/lifepo4Battery.php
+++ frontend/views/column/lifepo4Battery.php
@@ -15,7 +15,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、磷酸铁锂电池特点</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <ul>
                         <li>安全性能好，穿刺不爆炸，过充时不燃烧不爆炸；</li>
                         <li class="section10">循环寿命好，磷酸铁锂电池循环寿命可达2000次以上；</li>
@@ -26,7 +26,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、应用领域</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                  <p>动力储能、特种设备、机器人、AGV、轨道交通、医用设备、应急后备、电力通讯等。</p>
                 </div>
             </section>
@@ -99,33 +99,33 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
             <section>
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">磷酸铁锂电池优点</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、安全性能好</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                    <p>安全来自于正极材料的稳定性及可靠的安全性设计，磷酸铁锂电池包已经过严格的安全测试，即使在剧烈的碰撞中也不会产生爆炸。</p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、循环寿命长</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                   <p>
                       磷酸铁锂电池1C循环寿命普遍达2000次，甚至达到3500次以上， 而对于储能市场要求达到4000-5000次以上，高于其他种类锂电池。
                   </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>3、高温性能好</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         磷酸铁锂电池热峰值可达350~500℃，工作温度范围宽广(-20~+75℃)，高温(60℃)情况下仍可以放出100%容量。
                     </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>4、充电快速</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         使用专用充电器，1.5C充电40分钟即可以使电池充满。
                     </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>5、绿色环保</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         磷酸铁锂电池绿色环保，无毒，无污染，原材料来源广泛，价格便宜。
                     </p>
@@ -136,7 +136,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
             <section>
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">磷酸铁锂电池电压和容量</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、磷酸铁锂电池电压</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         单体磷酸铁锂电池的标称电压3.2V，充电电压3.6V，放电截止电压2.0V。
                     </p>
@@ -156,7 +156,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、磷酸铁锂电池容量</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>磷酸铁锂电池组的容量是根据并联在一起的电芯容量和数量确定，一般是根据用电设备的具体要求来确定，磷酸铁锂电芯并联的越多，容量越大。</p>
                     <p class="section10">常见的磷酸铁锂电池组容量有10ah,20ah,40ah,50ah,100ah, 200ah, 400ah等。</p>
                 </div>
@@ -165,7 +165,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
             <section>
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">磷酸铁锂电池结构和工作原理</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、磷酸铁锂电池结构</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         由图所示，左边是橄榄石结构的LiFePO4作为电池的正极，由铝箔与电池正极连接，中间是聚合物的隔膜，它把正极与负极隔开，但锂离子Li+可以通过而电子e-不能通过，右边是由碳（石墨）组成的电池负极，由铜箔与电池的负极连接。
                     </p>
@@ -176,7 +176,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、磷酸铁锂电池工作原理</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         LiFePO4电池在充电时，正极中的锂离子Li+通过聚合物隔膜向负极迁移；在放电过程中，负极中的锂离子Li+通过隔膜向正极迁移。锂离子电池就是因锂离子在充放电时来回迁移而命名的。
                     </p>
@@ -186,14 +186,14 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
             <section>
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">磷酸铁锂电池充电器</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1.LiFePO4 Battery Charging</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         磷酸铁锂电池组的充电建议使用CCCV充电方式，即先恒流后恒压。恒流建议0.3C。恒压建议3.65。
                     </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、磷酸铁锂电池和锂离子电池充电器一样吗</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <div class="p_ul">
                         <ul class="small-size light1-8">
                            <li>两种电池的充电方式都是先恒流再恒压（CCCV），但恒压点不一样。</li>
@@ -204,14 +204,14 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>3、用太阳能给磷酸铁锂电池充电</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         太阳能板是不能直接给磷酸铁锂电池充电的，因为太阳能电池板的电压是不稳定的，无法直接给磷酸铁锂电池充电，需要稳压电路，而且还要配套相应的磷酸铁锂电池充电电路才能充电。
                     </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>4、用发电机给磷酸铁锂电池充电</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         发电机是不能直接给磷酸铁锂电池充电的，因为发电机发出来的电为交流电或脉冲直流电，磷酸铁锂电池充电必须为稳压直流电。
                     </p>
@@ -309,13 +309,13 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                     </p>
                 </div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、材料对比</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         两种电池的负极、电解液和隔膜等均类似，最大的区别在于正极材料，并以此取名。
                     </p>
                 </div>
                 <div class="common_p">
-                    <div class="section30 col-md-12">
+                    <div class="section20 col-md-12">
                         <table cellspacing="0" width="100%" class="table-bordered">
                             <tbody>
                             <tr class="firstRow">
@@ -350,12 +350,12 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、能量密度</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         从电芯层面来讲，三元电池能量密度更高。磷酸铁锂正极材料的额定电压、理论比容量（mAh/g）均低于三元电池，且其能量密度已开发到“天花板”。
                     </p>
                 </div>
-                <div class="section30 col-md-12 common_p">
+                <div class="section20 col-md-12 common_p">
                     <table cellspacing="0" width="100%" class="table-bordered">
                         <tbody>
                         <tr class="firstRow">
@@ -393,7 +393,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                         </tbody>
                     </table>
                 </div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         注：电芯能量密度需结合电芯设计与工艺综合评估，表中值仅作参考。
                     </p>
@@ -401,7 +401,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>3、充电效率</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         三元锂电池在充电效率方面较磷酸铁锂电池有着非常大的优势。
                     </p>
@@ -409,21 +409,21 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>4、循环寿命</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         理论上，磷酸铁锂在寿命方面存在优势。其橄榄石晶体结构更稳定，膨胀更低、电化学反应更稳定。
                     </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>5、安全性</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         磷酸铁锂电池在安全方面具有无可比拟的优势。其正极电压低，不存在类似于三元的释氧热链式反应，热稳定温度可以达到300℃以上，而三元电池在150-200℃左右。
                     </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>6、经济性</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         价格上，目前磷酸铁锂LiFePO4有明显优势，原材料相对价格便宜，国内产业链相对成熟。
                     </p>
@@ -446,20 +446,20 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 <div class="container">
                     <div class="link_title size2 section20" id="battery-cell"><h3>1、磷酸铁锂电池常温环境下使用</h3></div>
                     <div class="row col-md-12">
-                        <ul class="ul_li_item_padding section60 m_section30">
+                        <ul class="ul_li_item_padding section60 m_section20">
                             <div class="item col-md-4">
                                 <li>
                                     <div class="title"><strong>小电流充放电</strong></div>
                                     <div class="text section10 light1-5"><p>在这一领域应用上磷酸铁锂电池正常使用的话，基本都是2000次以上的循环寿命；小型锂电池厂商，质量稍次一点的也有1000次以上的循环寿命；</p></div>
                                 </li>
                             </div>
-                            <div class="item col-md-4 m_section30">
+                            <div class="item col-md-4 m_section20">
                                 <li>
                                     <div class="title"><strong>高倍率稳定充放电使用</strong></div>
                                     <div class="text section10 light1-5"><p>高倍率放电的应用，大多数是动力型锂电池，多数是应用在给电机提供动力的应用上。由于磷酸铁锂电池多数在高负荷状态下运行，加快了电池材料衰减时间，循环寿命也在800次左右。</p></div>
                                 </li>
                             </div>
-                            <div class="item col-md-4 m_section30">
+                            <div class="item col-md-4 m_section20">
                                 <li>
                                     <div class="title"><strong>高倍率不稳定充放电使用</strong></div>
                                     <div class="text section10 light1-5"><p>这种情况下使用的磷酸铁锂电池，寿命会更短，只有300次左右。</p></div>
@@ -472,26 +472,26 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
             <div class="part2 pp">
                 <div class="container">
                     <div class="link_title size2 section20" id="battery-cell"><h3>2、磷酸铁锂电池高温环境下使用</h3></div>
-                    <div class="common_p section30">
+                    <div class="common_p section20">
                         <p>
                             磷酸铁锂电池高温性能目前不是很成熟，运行温度-20℃到125℃，这个温度范围为理论值，实际应用温度范围要小些。
                         </p>
                     </div>
                     <div class="row col-md-12">
-                        <ul class="section60 ul_li_item_padding m_section30">
+                        <ul class="section60 ul_li_item_padding m_section20">
                             <div class="item col-md-4">
                                 <li>
                                     <div class="title"><strong>小电流充放电</strong></div>
                                     <div class="text section10 light1-5"><p>在这一领域应用上磷酸铁锂电池正常使用的话，如果是电池品牌厂商实力比较强的，质量好的，基本都是1000次以上的循环寿命；小型锂电池厂商，质量稍次一点的也有500次以上的循环寿命；因为高温下使用，对电池损害是比较大的。</p></div>
                                 </li>
                             </div>
-                            <div class="item col-md-4 m_section30">
+                            <div class="item col-md-4 m_section20">
                                 <li>
                                     <div class="title"><strong>高倍率稳定充放电使用</strong></div>
                                     <div class="text section10 light1-5"><p>由于磷酸铁锂电池多数在高负荷状态下运行，加快了电池材料衰减时间，循环寿命是急剧下降的，质量差的电芯，可能也就300次左右的循环；实力强的电池品牌厂家，在设备技术和材料应用上会比较好，电芯质量就会好些，但循环寿命也就在500次左右。</p></div>
                                 </li>
                             </div>
-                            <div class="item col-md-4 m_section30">
+                            <div class="item col-md-4 m_section20">
                                 <li>
                                     <div class="title"><strong>高倍率不稳定充放电使用</strong></div>
                                     <div class="text section10 light1-5"><p>高温加放电倍率不稳定运行，对电池损害更大，循环使用寿命比较低，对几家电池厂家电芯做测试发现，250到300次这样，电池基本使用不了了。</p></div>
@@ -504,7 +504,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
             <div class="part3 pp">
                 <div class="container">
                     <div class="link_title size2 section20" id="battery-cell"><h3>3、磷酸铁锂电池低温环境下使用</h3></div>
-                    <div class="common_p section30">
+                    <div class="common_p section20">
                         <p>
                             低温环境下对磷酸铁锂电池性能影响要比高还要大，就目前市场情况来看，磷酸铁锂电池在低于-20℃以下到-40℃工作，寿命明显下降，循环使用寿命在300次左右。
                         </p>
@@ -515,15 +515,15 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 <div class="container">
                     <div class="link_title size2 section20" id="battery-cell"><h3>4、磷酸铁锂电池循环寿命影响因素</h3></div>
                     <div class="common_p section10">
-                        <li class="col-md-12 section30"><strong>充放电</strong></li>
+                        <li class="col-md-12 section20"><strong>充放电</strong></li>
                         <p class="section10 col-md-12">
                             选择充电器时，最好使用具备正确终止充电装置切断的充电器，以免磷酸铁锂电池因过充而缩短使用寿命。一般来说，慢速充电较快速充电更能延长电池的使用寿命。
                         </p>
-                        <li class="col-md-12 section30"><strong>放电深度</strong></li>
+                        <li class="col-md-12 section20"><strong>放电深度</strong></li>
                         <p class="section10 col-md-12">
                             放电的深度是影响磷酸铁锂电池寿命的主要因素，放电的深度越高，磷酸铁锂电池的寿命就越短。换句话说，只要降低放电深度，就能大幅延长锂磷酸铁锂电池的使用寿命。因此，我们应避免将锂电池UPS过放至极低的电压。
                         </p>
-                        <li class="col-md-12 section30"><strong>工作环境</strong></li>
+                        <li class="col-md-12 section20"><strong>工作环境</strong></li>
                         <p class="section10 col-md-12">
                             若磷酸铁锂电池长时间在高温下使用，会令其电极活性衰减，缩短使用寿命，所以，尽量保持在适宜的操作温度是延长磷酸铁锂电池寿命的好方法。
                         </p>
@@ -543,7 +543,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                     </p>
                 </div>
                 <div class="common_p">
-                    <li class="col-md-12 section30"><strong>火法回收工艺</strong></li>
+                    <li class="col-md-12 section20"><strong>火法回收工艺</strong></li>
                 </div>
                 <div class="common_p">
                     <p class="section10 col-md-12">
@@ -551,7 +551,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                     </p>
                 </div>
                 <div class="common_p">
-                    <li class="col-md-12 section30"><strong>湿法回收工艺</strong></li>
+                    <li class="col-md-12 section20"><strong>湿法回收工艺</strong></li>
                 </div>
                 <div class="common_p">
 
