398b7bec58e1bb7aa1eb671cacec317676b574aa
mac <635442166@qq.com>
2020-10-23 09:52:23 +0800

    特殊文章样式修改

--- frontend/views/column/lithiumPolymer.php
+++ frontend/views/column/lithiumPolymer.php
@@ -19,7 +19,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">聚合物锂电池的分类</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、按结构分</h3></div>
                 <div class="common_p">
-                    <li class="font-weight section30">卷绕式：</li>
+                    <li class="font-weight black_color section20">卷绕式：</li>
                 </div>
                 <div class="common_p">
 
@@ -28,7 +28,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                     </p>
                 </div>
                 <div class="common_p">
-                    <li class="font-weight section10">叠片式：</li>
+                    <li class="font-weight black_color section10">叠片式：</li>
                 </div>
                 <div class="common_p">
                     <p class="small-size section5">
@@ -38,7 +38,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、按电解质分类</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <ul>
                         <li>凝胶聚合物电解质锂离子电池，它是在固体聚合物电解质中加入添加剂提高离子电导率，使电池可在常温下使用；</li>
                         <li class="section10">固体聚合物电解质锂离子电池，电解质为聚合物与盐的混合物，在常温下的离子电导率低，适于高温使用；</li>
@@ -139,7 +139,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">聚合物锂电池参数</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1.Voltage</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                             <li>标称电压：3.7V</li>
                             <li>充电电压：4.1V~5V</li>
@@ -150,7 +150,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、容量</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                   <p>
                       聚合物锂电池的容量取决于电池的厚度、宽度与长度。另外还与电池的材料及大小有关。<br>
                       快速估算聚合物电池的容量，常采用的估算公式（仅用估算）：<br>
@@ -159,14 +159,14 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>3、内阻</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         内阻是指锂电池在工作时，电流流过锂电池内部所受到的阻力，主要由电极材料、电解液、隔膜电阻及各部分零件的接触电阻组成，与电池的尺寸、结构、装配等有关。
                     </p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>4、充电倍率</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         <strong>充电倍率=充电电流/额定容量</strong><br>
                         最大充电倍率是最安全的，但不是最好的充电倍率，较低的充电倍率可以更好的延长电池的使用寿命。
@@ -174,7 +174,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>5、放电倍率</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>
                         <strong>放电倍率=放电电流/额定容量</strong><br>
                         放电率就是保持电池可以安全放电的速度，是指电池在规定的时间内放出其额定容量时所需要的电流值，在数据值上等于电池额定容量的倍数，通常用字母C来表示。
@@ -199,7 +199,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、充电标准</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                             <li>聚合物锂电池的充电器必须能够恒流恒压充电；</li>
                             <li>充电时的单体电池必须在1C5A以下；</li>
@@ -211,7 +211,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、放电标准</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                             <li>放电电流小于2C5A；</li>
                             <li>单体电池放电终止电压不小于2.75V。</li>
@@ -223,7 +223,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>3、存储标准</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                             <li>电池一般都是属于易燃易爆品，放置的地方应该是避免高温且可以通风的地方。</li>
                             <li>电池长期存储环境为：-20℃~35℃，相对湿度为45%~75%。</li>
@@ -240,7 +240,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 <div class="common_p section60">
                     <p>聚合物锂电芯组装成组的过程称为PACK，可以是单只电池，也可以是多个电芯通过串并联的方式组合成锂电池模组等。</p>
                 </div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>聚合物锂电池PACK包括电池组、汇流排、软连接、保护板、外包装、输出（包括连接器）、青稞纸、塑胶支架等辅助材料这几项共同组成PACK。</p>
                 </div>
 
@@ -274,7 +274,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>常用电池插头</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                             <li>Molex 2510、Molex 51021、Molex 78172</li>
                             <li>JST SHR、JST ACHR、JST PHR、JST XHP、JST SYR、JST SYP、JST ZHR
@@ -286,7 +286,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>常用航模插头</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                             <li>EC3,EC5,EC8,XT30,XT60,XT90</li>
                         </ul>
@@ -298,7 +298,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">聚合物锂电池膨胀</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、聚合物锂电池膨胀的原因</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                             <li><strong>封装不良</strong>:制作过程中空气水分进入电芯内部，引起电解液分解产生气体。</li>
                             <li><strong>电芯含水超标</strong>:在工序过程中，一旦水含量超标，电解液会失效产生气体。</li>
@@ -313,7 +313,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 </div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、膨胀后的电池修复和使用</h3></div>
                 <div class="common_p">
-                    <div class="p_ul section30">
+                    <div class="p_ul section20">
                         <ul class=" small-size">
                           <li>如果手机电池已经被充的鼓起来的话，用手指先找到电池的空隙，用针对将戳一个小洞，让里面的空气跑出来就行了。</li>
                           <li>可以重新购置新的聚合物锂电池包来替换。在充电过程中，应当避免过充，既可以保护电池不受伤害，还节能环保。</li>
@@ -327,12 +327,12 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
             <section>
                 <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">锂聚合物电池处理</h2></div>
                 <div class="link_title size2 section60" id="battery-cell"><h3>1、合理利用废旧聚合物锂电池，避免对环境造成污染。</h3></div>
-                <div class="common_p section30">
+                <div class="common_p section20">
                     <p>聚合物锂电池被认为是相对环保的储能方式，但报废的聚合物锂电池如果回收和处理不当也会对环境造成污染。</p>
                 </div>
 
                 <div class="link_title size2 section60" id="battery-cell"><h3>2、废旧聚合物锂电池主要有害物质</h3></div>
-                <section class="section30">
+                <section class="section20">
                     <table cellspacing="0" width="100%" class="table-bordered">
                         <tbody>
                         <tr class="firstRow">
@@ -379,7 +379,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
                     <div class="link_title size2 section60" id="battery-cell"><h3>3、世界各国对废旧聚合物电池处理现状</h3></div>
                     <div class="common_p">
-                        <div class="p_ul section30">
+                        <div class="p_ul section20">
                             <ul class=" small-size">
                                <li>德国、美国已立法，开始做到全部收集，分类处理。</li>
                                <li>日本实行“3R计划”，变“大量生产、大量消费、大量废弃”为“循环、降低、再利用”。</li>
@@ -390,7 +390,7 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
 
                     <div class="link_title size2 section60" id="battery-cell"><h3>4、如何处理废旧聚合物锂电池</h3></div>
                     <div class="common_p">
-                        <div class="p_ul section30">
+                        <div class="p_ul section20">
                             <ul class=" small-size">
                                 <li>如果附近有废旧电池回收机构，请交给他们。</li>
                                 <li>如果附近没有废旧电池回收机构，且电池数量比较多，可以自行联络当地环保局或者邮寄到其他城市的回收机构。</li>
@@ -405,17 +405,17 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                 <section>
                     <div class="common_title size1 section70" id="basic-structure-of-18650"><h2 class="light1-5">购买锂聚合物电池的注意事项</h2></div>
                     <div class="link_title size2 section60" id="battery-cell"><h3>1、品牌</h3></div>
-                    <div class="common_p section30">
+                    <div class="common_p section20">
                         <p>选购聚合物锂电池时应认准知名品牌，电池的品质可以得到保障，市面上很多小品牌电池价格虽然便宜，但是寿命短、品质不稳定。</p>
                     </div>
 
                     <div class="link_title size2 section60" id="battery-cell"><h3>2、容量</h3></div>
-                    <div class="common_p section30">
+                    <div class="common_p section20">
                         <p>看是否有明确标注容量。无明确标示容量（如1000mAh或者许1000毫安培时刻）的电池组组 ，很有可以就是使用劣质电池组组或者许回收电池组，没有质量保证。</p>
                     </div>
 
                     <div class="link_title size2 section60" id="battery-cell"><h3>3、保护板</h3></div>
-                    <div class="common_p section30">
+                    <div class="common_p section20">
                         <p>无保护电路板，聚合物电池就有变形、漏液的危险。在市场竞争下，各家寻求更低价位的保护电路板，或者根本省略了这个装置，使得市面上充斥着有爆炸危险的聚合物电池。</p>
                     </div>
                 </section>
